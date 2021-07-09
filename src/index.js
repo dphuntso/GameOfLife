89b6f3fe8a9ddcdd6e409682788e9b4f76e1971f
Dorjee Phuntsok <dorjeephuntsok@Dorjees-MacBook-Pro.local>
2021-07-08 23:47:42 -0700

    transfer

--- src/index.js
+++ src/index.js
@@ -39,15 +39,50 @@ function setRules(row, column, dead, alive) {
 }
 
 function createTable(rules) {
+    let str = "";
+    let total = "";
     for(let i=0; i < rules.ROW; i++) {
-        let tr = document.createElement("tr");
+        str += "<tr>"
+        // let tr = document.createElement("tr");
         // tr.style.width = "100%";
         for (let col=0; col < rules.COLUMN; col++) {
-            let td = document.createElement("td");
-            tr.appendChild(td);
+            str += "<td></td>";
+            // let td = document.createElement("td");
+            // tr.appendChild(td);
         }
-        document.getElementById("table").appendChild(tr);
+        str += "</tr>"
+        total += str;
+        str = "";
+        // document.getElementById("table").appendChild(tr);
+    }
+    document.getElementById("table").innerHTML = total;
+}
+
+function drawBoard(board, rules) {
+    var table = document.getElementById("table");
+    for (var i = 0, row; row = table.rows[i]; i++) {
+        //iterate through rows
+        //rows would be accessed using the "row" variable assigned in the for loop
+        for (var j = 0, col; col = row.cells[j]; j++) {
+            if (board[i][j] == rules.DEAD) {
+                row.cells[j].style.backgroundColor = "white";
+            } else {
+                row.cells[j].style.backgroundColor = "black";
+            }
+            //iterate through columns
+            //columns would be accessed using the "col" variable assigned in the for loop
+        }  
     }
 }
+
+function nextState(boards, rules) {
+    let {currentBoard, nextBoard} = boards;
+    processBoard(currentBoard, nextBoard, rules);
+    drawBoard(nextBoard, rules);
+    let temp = currentBoard;
+    boards.currentBoard = nextBoard;
+    boards.nextBoard = temp;
+}
+
 
 // test();
